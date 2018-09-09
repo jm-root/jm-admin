d1ae3595e10221d3bde9bf62bbaeeb9106e792f4
jamma <jeff@jamma.cn>
2018-09-09 11:14:06 +0800

    jm-sdk

--- src/apps/sdk/index.js
+++ src/apps/sdk/index.js
@@ -7,13 +7,13 @@ const sdk = new Sdk(config);
 const router = ms.router();
 ms.client({ uri: config.api }).then(doc => {
   router.use(doc);
-  this.router = router;
+  sdk.router = router;
 });
 
 const { logger } = sdk;
 logger.level = config.logLevel || 'info';
 logger.info('config', config);
-sdk.login = async () => {
+sdk.login = async function() {
   const doc = await this.passport.login('root', '123');
   return doc;
 };
